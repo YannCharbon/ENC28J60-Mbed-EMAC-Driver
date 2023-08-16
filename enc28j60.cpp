73ebc7ed5c624a7ee1cb887e44ae053873a45f87
YannCharbon <yann.charbon@ik.me>
2023-08-16 12:00:35 +0200

    Append copyright

--- enc28j60.cpp
+++ enc28j60.cpp
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2019 Tobias Jaster
  *
- * Modified by Zoltan Hudak
+ * Modified by Zoltan Hudak and Yann Charbon
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
